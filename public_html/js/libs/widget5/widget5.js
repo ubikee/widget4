87a0a58b9cca738a7bedb5d0aa13cb73195f12b5
Ernesto Roldan <jeroldan@atsistemas.com>
2014-05-20 12:06:35 +0200

    innerHTML

--- public_html/js/libs/widget5/widget5.js
+++ public_html/js/libs/widget5/widget5.js
@@ -22,76 +22,45 @@ function supportImports() {
     return 'import' in document.createElement('link');
 }
 
-Node.prototype.offSpring = function() {
-    var n, a = [], walk = document.createTreeWalker(this, NodeFilter.SHOW_ELEMENT, null, false);
-    while (n = walk.nextNode())
-        a.push(n);
-    return a;
-};
-
-/*
- * 
- * @param {type} contentNode
- * @param {type} model
- * @returns {undefined}
- */
-function parseContent(contentNode, model) {
-
-    contentNode.offSpring().forEach(function(node) {
-
-        var re = /{{[a-z]*}}/g;
-
-        if (node.attributes) {
-            for (var i = 0; i < node.attributes.length; i++) {
-                var attribute = node.attributes.item(i);
-                var matches = attribute.value.match(re);
-                if (matches) {
-                    matches.forEach(function(match) {
-                        var field = match.slice(2, match.length - 2);
-                        if (model.getNamedItem(field)) {
-                            console.log('replace: ' + node.id + '.' + field + ' --> ' + model.getNamedItem(field).value);
-                            var newStr = attribute.value.replace(match, model.getNamedItem(field).value);
-                            node.setAttribute(attribute.nodeName, newStr);
-                        }
-                    });
-                }
-            }
-        }
-
-        if (node.textContent) {
-            var matches = node.textContent.match(re);
-            if (matches) {
-                matches.forEach(function(match) {
-                    var field = match.slice(2, match.length - 2);
-                    if (model.getNamedItem(field)) {
-                        console.log('replacecc: ' + node + '.' + field + ' --> ' + model.getNamedItem(field).value);
-                        var newStr = node.textContent.replace(match, model.getNamedItem(field).value);
-                        node.textContent = newStr;
-                    }
-                });
-            }
-        }
-        
-    });
-}
-
 /**
  * 
  * @param id
  * @param elem
  */
-function CustomElement(id, elem) {
+function CustomElement(src, elem) {
+
+    var src = src;
+    var elem = elem;
+    
+    var tagName = (function (src) {
+        var _link = document.querySelector('link[href$="' + src + '"]');
+        return _link.id === "" ? 'w5-javasaurio' : _link.id;
+    })(src);
 
     var proto = Object.create(HTMLElement.prototype);
 
     proto.createdCallback = function(arg) {
+        
         console.log('created');
-        var link = document.querySelector('link[tag="'+id+'"]').import;
-        var tmpl = link.querySelector('#' + id);
+        
+        var link = document.querySelector('link[href$="' + src + '"]').import;
+        var tmpl = link.querySelector('template');
         var content = document.importNode(tmpl.content, true);
-        parseContent(content, this.attributes);
+        
+        for (var i = 0, length = content.children.length; i < length; i++) {
+            var templ = content.children[i].innerHTML;
+            for (var j = 0, l2 = this.attributes.length; j< l2; j++ ) {
+                var a = this.attributes.item(j);
+                templ = templ.replace('{{'+a.nodeName+'}}', a.value);
+            }
+            content.children[i].innerHTML = templ;
+        }
+
         elem.shadowRoot = this.createShadowRoot();
         elem.shadowRoot.appendChild(content);
+        
+        // TODO : bindings
+        
         if (elem.created)
             elem.created();
     };
@@ -119,6 +88,5 @@ function CustomElement(id, elem) {
         elem.blink();
     };
 
-    return document.registerElement(id, {prototype: proto});
+    return document.registerElement(tagName, {prototype: proto});
 }
-
